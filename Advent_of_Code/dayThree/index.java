6ebb2b05d74e2a2c45ef8450cde7a62751a02ceb
marcdfrench <marcdfrench@gmail.com>
2025-08-26 08:22:55 -0500

    day 3 done

--- Advent_of_Code/dayThree/index.java
+++ Advent_of_Code/dayThree/index.java
@@ -8,32 +8,54 @@ import java.util.regex.Pattern;
 
 public class index {
     public static void main(String[] args) throws FileNotFoundException {
+        // Scanner File input
         Scanner sc = new Scanner(new FileReader(
                 "C:\\Users\\marc.french\\Desktop\\ReLearning_JS&Java\\Advent_of_Code\\dayThree\\input.txt"));
+        
+        // String Builder reads each line and appends them together
         StringBuilder sb = new StringBuilder();
         while (sc.hasNextLine()) {
             sb.append(sc.nextLine());
         }
         String str = sb.toString();
+        
+        // regex
         String regex = "\\([0-9]+,[0-9]+\\)";
         Pattern pattern = Pattern.compile(regex);
         int total = 0;
+        boolean toggle = true;
+        
+        // loop through the string
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == 'm' && str.charAt(i + 1) == 'u' && str.charAt(i + 2) == 'l') {
-                int endIndex = Math.min(i + 12, str.length());
-                Matcher matcher = pattern.matcher(str.substring(i + 3, endIndex));
-                if (matcher.find()) {
-                    String found = (matcher.group().replace("(", "").replace(")", ""));
-                    String[] result = found.split(",");
-                    int x = Integer.parseInt(result[0]);
-                    int y = Integer.parseInt(result[1]);
-                    int z = x * y;
-                    total += z;
+            // Check for do()
+            if (str.startsWith("do()", i)) {
+                toggle = true;
+            }
+            // Check for don't()
+            if (str.startsWith("don't()", i)) {
+                toggle = false;
+            }
+            if (toggle == true) {
+                // Check for mul
+                if (str.startsWith("mul", i)) {
+                    // check if format after mul matches regex
+                    int endIndex = Math.min(str.length(), i + 12);
+                    Matcher matcher = pattern.matcher(str.substring(i + 3, endIndex));
+                    if (matcher.find()) {
+                        // Get the numbers from the regex match and remove the ()
+                        String found = (matcher.group().replace("(", "").replace(")", ""));
+                        System.out.println(found);
+                        String[] result = found.split(",");
+                        int x = Integer.parseInt(result[0]);
+                        int y = Integer.parseInt(result[1]);
+                        int z = x * y;
+                        total += z;
+                    }
+
                 }
-            } 
+            }
         }
         sc.close();
         System.out.println(total);
     }
-
 }
